89ef43366f6ce75b16c0012c29a482c125b7b178
Marcelo Martone <marcelomartonejr@gmail.com>
2022-02-21 13:13:51 -0300

    Exercícios de POO

--- Exercícios_17-02/src/Mains/Pessoa_main.java
+++ Exercícios_17-02/src/Mains/Pessoa_main.java
@@ -12,22 +12,22 @@ public class Pessoa_main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int opc = 0;
+        int opc;
 
         Pessoa pessoa = new Pessoa("Marcelo", "Rua tals", "99 9999-9999");
         Fornecedor fornecedor = new Fornecedor("Linx", "Endereço da Linx","11 9999-9999", 200000, 150000);
-        Empregado empregado = new Empregado("Luiz", "Rua do Luiz", "11 1111-1111", 001, 3000, 3);
-        Administrador administrador = new Administrador("Luiza", "Rua da Luiza", "12 1111-2111", 001, 5000, 3, 300);
+        Empregado empregado = new Empregado("Luiz", "Rua do Luiz", "11 1111-1111", 1, 3000, 3);
+        Administrador administrador = new Administrador("Luiza", "Rua da Luiza", "12 1111-2111", 1, 5000, 3, 300);
         Operario operario = new Operario("Robson", "Rua do Robson", "13 1113-1111", 734, 2000, 3, 1000, 20);
         Vendedor vendedor  = new Vendedor("Walderrama", "Rua do Walderrama", "15 1153-1111", 664, 2500, 3, 15000, 30);
 
         System.out.println("quais dados deseja verificar?");
-        System.out.println("1 - Pessoa física;");
-        System.out.println("2 - Fornecedor;");
-        System.out.println("3 - Empregado;");
-        System.out.println("4 - Administrador;");
-        System.out.println("5 - Operario;");
-        System.out.println("6 - Vendedor;");
+        System.out.println("1 - Pessoa física");
+        System.out.println("2 - Fornecedor");
+        System.out.println("3 - Empregado");
+        System.out.println("4 - Administrador");
+        System.out.println("5 - Operario");
+        System.out.println("6 - Vendedor");
         opc = sc.nextInt();
 
         switch (opc){
